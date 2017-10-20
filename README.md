26043b88e610607ee8bd9d2163a03a83c01f4aee
CS <chance@snapshotinteractive.com>
2017-10-20 15:23:27 -0500

    add files and readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # WordPress + Foundation Columns Nav Menu
-A WP walker to support a footer menu where top-level menu items are laid out as columns.
+A WP walker to support a footer menu where top-level menu items are laid out as columns in Foundation's Float Grid.
+
+Load the Walker in `functions.php` or in a plugin, then add the menu to your template file as such:
+
+```php
+$args = [
+  'theme_location'   => 'footer-navigation',
+  'container'        => 'div',
+  'container_class'  => "menu-{$menu->slug}-container columns small-6 medium-3 large-2",
+  'menu_class'       => 'footer-menu',
+  'walker'           => new Walker_Nav_Menu_Footer,
+];
+wp_nav_menu( $args );
+```
+
+The nav menu will need to be placed inside of a `row` element, which is excluded from the walker to accommodate items outside of the menu that you might want in the same row. This will produce the following layout, often used in footers:
+
+```
+Top Level Item 1    Top Level Item 2		Top Level Item 3
+--------						--------						--------
+Child item 1				Child item 1				Child item 1
+Child item 2				Child item 2			  Child item 2
+Child item 3				Child item 3				Child item 3
+```
+
+You can easily modify the column layouts to use the XY grid or take up different numbers of grid columns -- just make sure to modify the classes in both the walker and the `wp_nav_menu` function.
---
+++ Walker-Nav-Menu-Footer.php
@@ -0,0 +1,50 @@
+<?php
+class Walker_Nav_Menu_Footer extends Walker_Nav_Menu {
+
+    // Set counter; later we'll use it to check for the first parent element
+    public static $counter = 0;
+
+    function start_el( &$output, $item, $depth, $args ) {
+
+        global $wp_query;
+
+        if ( ! isset( $this->current_menu ) )
+            $this->current_menu = wp_get_nav_menu_object( $args->menu );
+
+        // Set indent for sub-menu items
+        $indent      = ( $depth > 1 ) ? str_repeat( "\t", $depth - 1 ) : '';
+
+        // Set class values for menu items
+        $classes     = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[]   = 'menu-item-' . $item->ID;
+        $class_names = $value = '';
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+        $class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+        // Set ID value for menu items
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+        $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+        // Check if you are displaying a top-level element to increment counter
+        if ( $item->menu_item_parent === '0' ) {
+            $this::$counter++;
+        }
+
+        if ( $depth === 0 && $this::$counter > 1 ) {
+            $output .= $indent . '</li></ul></div><div class="columns small-6 medium-3 large-2"><ul><li' . $id . $value . $class_names .'>';
+        }
+
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+
+        $item_output  = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+}
